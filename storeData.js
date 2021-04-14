a0acd795265e2826fb080d1141f25e56f386e7d2
vikashchander <vikashchander78384@gmail.com>
2021-04-14 16:03:52 +0530

    storeData in json form

---
+++ storeData.js
@@ -0,0 +1,33 @@
+
+const puppeteer = require('puppeteer')
+const screenshot = 'shopping.png'
+const fs = require("fs");
+try {
+  (async () => {
+    const browser = await puppeteer.launch({ headless: false,defaultViewport: false,
+        args: ["--start-maximized"]})
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.goto('https://www.ajio.com/', { waitUntil: 'networkidle2' })
+    await page.waitForSelector('input[name="searchVal"]');
+    await page.type('input[name="searchVal"]',"shoes");
+    await  page.waitForSelector('button.rilrtl-button');
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('div.preview');
+    //  let data = await page.$$('div.preview div');
+    //  console.log(data);
+    let storeLinks=await page.$$eval('img.rilrtl-lazy-img.rilrtl-lazy-img-loaded', postLinks => postLinks.map(data =>
+        {
+            return {
+                'imgSrc': data.src,
+                'imgAlt':data.alt
+            }
+        }));
+   console.log(storeLinks);
+   fs.writeFile('./data.json', JSON.stringify(storeLinks), err => err ? console.log(err): null);
+    await page.screenshot({ path: screenshot }  )
+    console.log('See screen shot: ' + screenshot)
+  })()
+} catch (err) {
+  console.error(err)
+}   
